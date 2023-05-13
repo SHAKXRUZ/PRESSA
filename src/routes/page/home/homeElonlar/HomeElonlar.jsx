ac78a50665344cff72adddbfbffd9e2e8b356070
shaxruz25 <shaxruzismoilov250@gmail.com>
2023-05-13 15:34:18 +0500

    home update margin

--- src/routes/page/home/homeElonlar/HomeElonlar.jsx
+++ src/routes/page/home/homeElonlar/HomeElonlar.jsx
@@ -1,6 +1,12 @@
 import "./HomeElonlar.css";
 const HomeElonlar = () => {
-  return <div>HomeElonlar</div>;
+  return <div className="hoem_elonlar">
+    <div className="home_elonlar_container">
+      <div className="home_elonlar_content">
+        
+      </div>
+    </div>
+  </div>;
 };
 
 export default HomeElonlar;
