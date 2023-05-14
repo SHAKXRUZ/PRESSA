6219935ae68a11ee212d886cebb81a8fc094e965
shaxruz25 <shaxruzismoilov250@gmail.com>
2023-05-14 11:47:20 +0500

    admin login desktop full

--- src/admin/adminLogin/AdminLogin.jsx
+++ src/admin/adminLogin/AdminLogin.jsx
@@ -1,12 +1,49 @@
 import "./AdminLogin.css";
+import { Link } from "react-router-dom";
+import { AiOutlineUser } from "react-icons/ai";
+import { BiLockAlt } from "react-icons/bi";
+import LoginLogo from "../../assets/images/login_logo.png";
 const AdminLogin = () => {
   return (
     <div className="login">
       <div className="login_container">
-        <div className="login_content">
-          <form>
-            <h2 className="login_pages_title">Tizimga kirish</h2>
-            
+        <Link to="/">
+          <img className="login_logo" src={LoginLogo} alt="login_logo" />
+        </Link>
+        <div className="login_form_div">
+          <form className="login_form">
+            <h2 className="login_form_title">Tizimga kirish</h2>
+            <div className="login_form_input_div">
+              <span className="login_input_icons">
+                <AiOutlineUser className="login_input_icons" />
+              </span>
+              <input
+                autocomplete="off"
+                className="login_form_input"
+                type="email"
+                name="email"
+                placeholder="Login"
+                required
+              />
+            </div>
+            <div className="login_form_input_div">
+              <span className="login_input_icons">
+                <BiLockAlt className="login_input_icons" />
+              </span>
+              <input
+                className="login_form_input"
+                type="password"
+                name="password"
+                placeholder="Parol"
+                required
+              />
+            </div>
+            <p className="login_resit_password_text">Parolni unutdingizmi?</p>
+            <div className="login_btn_div">
+              <button type="submit" className="login_btn">
+                Kirish
+              </button>
+            </div>
           </form>
         </div>
       </div>
