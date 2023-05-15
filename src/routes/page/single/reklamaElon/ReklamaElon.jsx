664a614c12a3983987093b6e8db8fd02a139721b
shaxruz25 <shaxruzismoilov250@gmail.com>
2023-05-15 20:09:49 +0500

    create elon card

--- src/routes/page/single/reklamaElon/ReklamaElon.jsx
+++ src/routes/page/single/reklamaElon/ReklamaElon.jsx
@@ -1,6 +1,12 @@
 import "./ReklamaElon.css";
 const ReklamaElon = () => {
-  return <div>ReklamaElon</div>;
+  return <div className="reklama_elonlar">
+    <div className="reklama_elonlar_container">
+      <div className="reklama_elonlar_content">
+        
+      </div>
+    </div>
+  </div>;
 };
 
 export default ReklamaElon;
